cef033c773606b98fecec6e4d4f0d3ef8137f310
PC <PC@DESKTOP-V9GOQ3S>
2024-04-24 12:11:40 +0200

    MAIN

--- src/Inicio/David.java
+++ src/Inicio/David.java
@@ -3,6 +3,17 @@ package Inicio;
 public class David {
 	
 	public static void main(String[] arg) {
+			Scanner ent=new Scanner(System.in);
+			System.out.println("BUENASSS  T VASSS PERMA VALEEE?");
+			String mensaje=ent.nextLine();
+			if(mensaje.equalsIgnoreCase("DAVID PUTERO")) {
+				System.out.println("░░░░██ ]▄▄▄▄▄▄▃\r\n"
+						+ "▂▄▅█████▅▄▃▂\r\n"
+						+ "I█████████████].\r\n"
+						+ "◥⊙▲⊙▲⊙▲⊙▲⊙◤ \nBombardeen peru");
+			}else {
+				System.out.println("PERMABAAAAAAAN");
+			}
 		
 	}
 
